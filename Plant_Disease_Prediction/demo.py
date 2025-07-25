381ba42a268876d6d9f1bfc21d7f6c0e9526a6aa
Arghyadip Roy <arghyadiproy337@gmail.com>
2025-07-25 14:26:20 +0530

    Add demo.py

---
+++ Plant_Disease_Prediction/demo.py
@@ -0,0 +1,20 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+while True:
+    ret, frame = cap.read()
+    img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(img_rgb)
+    
+    if result.multi_hand_landmarks:
+        for handLms in result.multi_hand_landmarks:
+            mp_draw.draw_landmarks(frame, handLms, mp_hands.HAND_CONNECTIONS)
+    
+    cv2.imshow("Hand Tracking", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
